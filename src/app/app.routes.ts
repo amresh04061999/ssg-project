e228d502d762f6f1a6609e3491e5a2d415f1ff8d
Amresh Prasad <amresh1098@gmail.com>
2024-01-25 12:01:57 +0530

    feat:ssg project

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { PostComponent } from './component/post/post.component';
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'post', component: PostComponent },
+];
